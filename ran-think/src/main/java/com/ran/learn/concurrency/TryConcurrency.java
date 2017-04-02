0111a19d5e9515aef4531d2bc63ddaccb8192aca
zhangran <zhangran42@126.com>
2017-04-02 11:23:33 +0800

    try

---
+++ ran-think/src/main/java/com/ran/learn/concurrency/TryConcurrency.java
@@ -0,0 +1,31 @@
+package com.ran.learn.concurrency;
+
+public class TryConcurrency {
+
+    public static void main(String[] args) {
+        readFromDatabase();
+        writeToFile();
+    }
+
+    private static void readFromDatabase() {
+        try {
+            System.out.println("read from db");
+            Thread.sleep(1000 * 5);
+            System.out.println("read from db and handle it");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("handle success.");
+    }
+
+    private static void writeToFile() {
+        try {
+            System.out.println("write to file");
+            Thread.sleep(1000 * 5);
+            System.out.println("write file and handle it");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("handle success.");
+    }
+}
