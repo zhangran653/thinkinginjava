46d8a98fdfac1fd3f0b559a07051574e27f3068e
zhangran <zhangran42@126.com>
2017-04-02 16:14:56 +0800

    thread

---
+++ ran-think/src/main/java/com/ran/learn/concurrency/chapter03/CreateThread.java
@@ -0,0 +1,27 @@
+package com.ran.learn.concurrency.chapter03;
+
+public class CreateThread {
+
+    public static void main(String[] args) {
+        /**
+         * 1.创建Thread对象，默认构造器new Thread(),默认有一个name，已Thread-开头，从0开始计数 . Thread-0
+         * Thread-1 Thread-2
+         * 
+         */
+        Thread t1 = new Thread();
+        /**
+         * 2.如果构造Thread没有传Runnable接口或者没有覆写run方法，则不执行任何东西。反之执行逻辑
+         *
+         */
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("============");
+            }
+        };
+        t1.start();
+        t2.start();
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+    }
+}
