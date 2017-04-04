5014c3320e2be353ab05ff9e2877e6c0720c40a4
zhangran <zhangran42@126.com>
2017-04-04 13:29:36 +0800

    thread close

--- ran-think/src/main/java/com/ran/learn/concurrency/chapter07/ThreadService.java
+++ ran-think/src/main/java/com/ran/learn/concurrency/chapter07/ThreadService.java
@@ -4,8 +4,6 @@ public class ThreadService {
 
     private Thread executeThread;
 
-    private volatile boolean isFinished = false;
-
     /**
      * 思路：将任务线程设置为守护线程，执行线程只负责开启任务线程。执行线程需要join，让任务线程执行完成。当需要强行关闭时，由于执行线程此时join，即wait
      * 对其使用interrupt，捕获异常，完成生命周期，任务线程由于是守护线程，自然会关闭。
@@ -22,8 +20,7 @@ public class ThreadService {
                 runner.start();
                 try {
                     runner.join();
-                    isFinished = true;
-                    System.out.println("is finish " + isFinished);
+
                 } catch (InterruptedException e) {
                     System.out.println(
                             "execute thread is interrupted.ready to shutdown.");
@@ -35,7 +32,7 @@ public class ThreadService {
 
     public void shutdown(long mills) {
         long current = System.currentTimeMillis();
-        while (!isFinished) {
+        while (executeThread.isAlive()) {
 
             if ((System.currentTimeMillis() - current) >= mills) {
                 System.out.println("任务超时。");
@@ -44,6 +41,5 @@ public class ThreadService {
             }
         }
 
-        isFinished = false;
     }
 }
