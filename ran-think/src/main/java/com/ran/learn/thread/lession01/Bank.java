adfc58464fd5ea454ee7d1b3ecbaadf0b025d555
zhangran <zhangran42@126.com>
2017-10-24 23:15:30 +0800

    test

--- ran-think/src/main/java/com/ran/learn/thread/lession01/Bank.java
+++ ran-think/src/main/java/com/ran/learn/thread/lession01/Bank.java
@@ -6,13 +6,10 @@ package com.ran.learn.thread.lession01;
 public class Bank {
 
     public static void main(String[] args) {
-        final TicketWindow ticketWindow = new TicketWindow();
-        Thread t1 = new Thread(ticketWindow, "一号柜台");
-        Thread t2 = new Thread(ticketWindow, "二号柜台");
-        Thread t3 = new Thread(ticketWindow, "三号柜台");
 
-        t1.start();
-        t2.start();
-        t3.start();
+        String s= "(/:pig|/:rose)";
+        String words = "爱哭的舒服就好阿拉山口地方就/:pig,asjkdfa爱上的看/:rose";
+        words.replaceAll(s,"[表情]");
+        System.out.print(words);
     }
 }
