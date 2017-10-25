28e98c5f75d48336731af79c9fd7dc6157d8fe8e
zhangran <zhangran42@126.com>
2017-10-25 22:52:56 +0800

    d

---
+++ ran-think/src/main/java/com/ran/learn/lession2/ThreadInterrupt.java
@@ -0,0 +1,46 @@
+package com.ran.learn.lession2;
+
+import java.util.Objects;
+
+/**
+ * Created by zhangran on 2017/10/25.
+ */
+public class ThreadInterrupt {
+
+    private static final Object object = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                while (true) {
+//                    System.out.println("===" + this.isInterrupted());
+//                    try {
+//                        Thread.sleep(10);
+//                    } catch (InterruptedException e) {
+//                        System.out.println("收到打断信号");
+//                        e.printStackTrace();
+//                    }
+                    synchronized (object){
+                        try {
+                            object.wait(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                }
+            }
+        };
+
+        t.start();
+        Thread.sleep(100);
+
+        System.out.println(t.isInterrupted());
+        t.interrupt();
+        System.out.println(t.isInterrupted());
+
+    }
+}
