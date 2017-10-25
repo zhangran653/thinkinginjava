fa3277090e57df4ccec7ad6916f9c9785426d2d0
zhangran <zhangran42@126.com>
2017-10-25 23:02:25 +0800

    d

--- ran-think/src/main/java/com/ran/learn/lession2/ThreadInterrupt.java
+++ ran-think/src/main/java/com/ran/learn/lession2/ThreadInterrupt.java
@@ -10,37 +10,68 @@ public class ThreadInterrupt {
     private static final Object object = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread() {
+//        Thread t = new Thread() {
+//
+//            @Override
+//            public void run() {
+//                while (true) {
+////                    System.out.println("===" + this.isInterrupted());
+////                    try {
+////                        Thread.sleep(10);
+////                    } catch (InterruptedException e) {
+////                        System.out.println("收到打断信号");
+////                        e.printStackTrace();
+////                    }
+//                    synchronized (object){
+//                        try {
+//                            object.wait(10);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//
+//
+//                }
+//            }
+//        };
+//
+//        t.start();
+//        Thread.sleep(100);
+//
+//        System.out.println(t.isInterrupted());
+//        t.interrupt();
+//        System.out.println(t.isInterrupted());
 
+        Thread t = new Thread(){
             @Override
             public void run() {
-                while (true) {
-//                    System.out.println("===" + this.isInterrupted());
-//                    try {
-//                        Thread.sleep(10);
-//                    } catch (InterruptedException e) {
-//                        System.out.println("收到打断信号");
-//                        e.printStackTrace();
-//                    }
-                    synchronized (object){
-                        try {
-                            object.wait(10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
+                while (true){
 
                 }
             }
         };
-
+        Thread main = Thread.currentThread();
         t.start();
-        Thread.sleep(100);
+        Thread t2 = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                main.interrupt();
+                System.out.println("interrupt");
+            }
+        };
+        t2.start();
 
-        System.out.println(t.isInterrupted());
-        t.interrupt();
-        System.out.println(t.isInterrupted());
+        try {
+            t.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
