8a0e66986cdd676791ed0159606a908cda0b117d
zhangran <zhangran42@126.com>
2017-11-15 16:31:52 +0800

    d

---
+++ ran-think/src/main/java/com/ran/learn/lession3/ReadWriteLock.java
@@ -0,0 +1,48 @@
+package com.ran.learn.lession3;
+
+/**
+ * Created by zhangran on 2017/11/4.
+ */
+public class ReadWriteLock {
+    private int readingReaders = 0;
+    private int waitingReaders = 0;
+    private int writingWriters = 0;
+    private int waitingWriters = 0;
+
+    public synchronized void readLock() throws InterruptedException {
+        waitingReaders++;
+        try {
+            while (writingWriters > 0) {
+                this.wait();
+            }
+            readingReaders++;
+        } finally {
+            waitingReaders--;
+        }
+
+    }
+
+    public synchronized void readUnlock() {
+        readingReaders--;
+        this.notifyAll();
+    }
+
+    public synchronized void writeLock() throws InterruptedException {
+        waitingWriters++;
+        try {
+            while (readingReaders > 0 || waitingWriters > 0) {
+                this.wait();
+            }
+            writingWriters++;
+        } finally {
+            waitingWriters--;
+        }
+
+    }
+
+    public synchronized void writeUnlock() {
+        writingWriters--;
+        this.notifyAll();
+    }
+
+}
---
+++ ran-think/src/main/java/com/ran/learn/lession3/SharedData.java
@@ -0,0 +1,8 @@
+package com.ran.learn.lession3;
+
+/**
+ * Created by zhangran on 2017/11/5.
+ */
+public class SharedData {
+
+}
